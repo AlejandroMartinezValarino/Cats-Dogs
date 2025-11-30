b0743b3056f52cdc9d9e40f24776be610fb94a8e
Álex <amarval@tutamail.com>
2025-11-30 19:48:44 +0100

    fix: use Express 5 syntax for catch-all route

--- frontend/server.js
+++ frontend/server.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3000;
 // Servir archivos estáticos desde dist
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Para SPA: redirigir todas las rutas a index.html
-app.get('*', (req, res) => {
+// Para SPA: redirigir todas las rutas a index.html (Express 5 syntax)
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
